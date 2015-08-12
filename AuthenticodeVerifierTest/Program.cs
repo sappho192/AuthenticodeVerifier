894262304b5a330c86d33b7a06cdd9ce010c1605
Sappho (Tae In, Kim) <sappho192@wellbia.com>
2015-08-12 19:01:15 +0900

    Updated test code

--- AuthenticodeVerifierTest/Program.cs
+++ AuthenticodeVerifierTest/Program.cs
@@ -10,12 +10,19 @@ namespace AuthenticodeVerifierTest
             var verifier = new AuthenticodeVerifier.AuthenticodeVerifier();
             verifier.LoadTarget(@"C:\temp\dbk32.sys");
             Console.WriteLine(verifier.Verify());
+            verifier.CertificateInfo.PrintInfo();
+
             verifier.LoadTarget(@"C:\temp\msvcr110.dll");
             Console.WriteLine(verifier.Verify());
+            verifier.CertificateInfo.PrintInfo();
+
             verifier.LoadTarget(@"C:\temp\siegfried.exe");
             Console.WriteLine(verifier.Verify());
+            verifier.CertificateInfo.PrintInfo();
+
             verifier.LoadTarget(@"C:\temp\COMDLG32.OCX");
             Console.WriteLine(verifier.Verify());
+            verifier.CertificateInfo.PrintInfo();
         }
     }
 }
