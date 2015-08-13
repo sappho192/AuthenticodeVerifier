237c826a8efd8fb391da5374c031c940ad1de533
Sappho (Tae In, Kim) <sappho192@wellbia.com>
2015-08-13 16:39:59 +0900

    Revert "Removed commented PrintLineConsole() codes"
    This reverts commit 7971842af28fc4f302cda4369be544a7e028d787.

--- AuthenticodeVerifierTest/AuthenticodeVerifier/CertificateVerifier.cs
+++ AuthenticodeVerifierTest/AuthenticodeVerifier/CertificateVerifier.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Security.Cryptography;
@@ -18,8 +17,6 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
         public CertificateVerifier()
         {
             _certificateStore = new CertificateStore();
-            CertificateInfo = new CertificateInfo();
-            // 나머지 멤버 변수들은 Initalize()를 통해 늦은 초기화를 겪습니다.
         }
         public override bool Verify()
         {
@@ -40,10 +37,6 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
             return ResultPrimary || ResultAdvanced;
         }
 
-        /// <summary>
-        /// 검사 결과를 문자열로 반환합니다. Verify() 혹은 VerifyDirect()가 선행되어야 합니다.
-        /// </summary>
-        /// <returns></returns>
         public override string GetResult()
         {
             var stringBuilder = new StringBuilder();
@@ -53,19 +46,11 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
             return stringBuilder.ToString();
         }
 
-        /// <summary>
-        /// 검사 결과를 콘솔 창에 출력합니다. Verify() 혹은 VerifyDirect()가 선행되어야 합니다.
-        /// </summary>
         public override void PrintResult()
         {
             Console.Write(GetResult());
         }
 
-        /// <summary>
-        /// 검사할 대상을 지정합니다. true가 반환되면 Verify()를 호출해 검사를 진행하세요.
-        /// </summary>
-        /// <param name="filePath"></param>
-        /// <returns></returns>
         public override bool LoadTarget(string filePath)
         {
             if (!File.Exists(filePath)) return false;
@@ -73,75 +58,29 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
             return true;
         }
 
-        /// <summary>
-        /// X509Certificate2 객체를 가지고 있을 때 호출할 수 있습니다.
-        /// VerifyDirect()를 호출하면 검사를 수행합니다.
-        /// </summary>
-        /// <param name="target"></param>
-        public void LoadTargetDirect(X509Certificate2 target)
-        {
-            _mainCert = target;
-        }
-
-        /// <summary>
-        /// 인증서가 유효한지 검사합니다. LoadTargetDirect가 반드시 선행되어야 합니다.
-        /// </summary>
-        /// <returns></returns>
-        public bool VerifyDirect()
-        {
-            InitializeDirect();
-
-            ResultPrimary = _mainCert.Verify();
-            if (!ResultPrimary) ResultAdvanced = VerifyAdvanced();   // 기본 검증에 실패했을 때만 수행
-
-            GetCertInfo();
-            return ResultPrimary || ResultAdvanced;
-        }
-
-        /// <summary>
-        /// 하나의 인스턴스에 여러 개의 파일을 넣어 검사할 경우에 대비해 초기화 구문을 분리했습니다.
-        /// </summary>
         private void Initialize()
         {
+            _basicCert = null;  // 할당은 ExtractCert()에서...
             _mainCert = null;   // 할당은 ExtractCert()에서...
-            _parentCertURL = "null";
             _keyChain = new X509Chain();
             CertificateInfo = new CertificateInfo();
         }
 
-        /// <summary>
-        /// _mainCert가 이미 초기화된 상태이므로 다른 멤버 변수들만 초기화합니다.
-        /// </summary>
-        private void InitializeDirect()
-        {
-            _keyChain = new X509Chain();
-            CertificateInfo = new CertificateInfo();
-        }
-
-        /// <summary>
-        /// 지정한 파일에서 디지털 서명의 객체를 추출합니다.
-        /// </summary>
-        /// <returns></returns>
         private bool ExtractCert()
         {
             try
             {
-                _mainCert = new X509Certificate2(X509Certificate.CreateFromSignedFile(_targetPath));
-                _parentCertURL = GetParentCertURL(); // 부모 인증서의 주소를 얻음
-
-                return true;
+                _basicCert = X509Certificate.CreateFromSignedFile(_targetPath);
             }
             catch (CryptographicException)
             {
-                //("해당 파일에는 인증서가 없는 것 같습니다.");
+                //PrintLineConsole("해당 파일에는 인증서가 없는 것 같습니다.");
                 return false;
             }
+            _mainCert = new X509Certificate2(_basicCert);
+            return true;
         }
 
-        /// <summary>
-        /// 시스템의 기본 검증 정책을 통과하지 못했을 경우 세부적으로 다시 검증합니다.
-        /// </summary>
-        /// <returns></returns>
         private bool VerifyAdvanced()
         {
             _keyChain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
@@ -151,18 +90,19 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
             try
             {
                 var elementValid = _keyChain.Build(_mainCert);
-                //(string.Format("keyChain building status: {0}", elementValid));
+                //PrintLineConsole(string.Format("keyChain building status: {0}", elementValid));
                 if (elementValid == false)
                 {
                     foreach (X509ChainStatus chainStatus in _keyChain.ChainStatus)
                     {
-                        //(string.Format("keyChain error: {0} {1}", chainStatus.Status, chainStatus.StatusInformation));
+                        //PrintLineConsole(string.Format("keyChain error: {0} {1}", chainStatus.Status, chainStatus.StatusInformation));
                         if (chainStatus.Status != X509ChainStatusFlags.NotTimeValid) continue;
+                        //PrintLineConsole("현재 인증서는 만료되었습니다.");
                         AddCertNote("현재 인증서는 만료되었습니다.");
-
-                        // 인증서 기한은 만료되었으므로 부모 인증서는 만료되었는지 체크함
+                        // 인증서 기한은 만료되었는데 부모 인증서들은 유효한 경우
                         if (VerifyParentCert())
                         {
+                            //PrintLineConsole("부모 인증서는 유효합니다.");
                             AddCertNote("부모 인증서는 유효합니다.");
                             return true;
                         }
@@ -182,6 +122,10 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
         /// <returns>검증 결과</returns>
         private bool VerifyParentCert()
         {
+            //foreach (var chain in _keyChain.ChainElements)
+            //{
+            //    Console.WriteLine("체인 유효성: " + chain.Certificate.Verify());
+            //}
             if (VerifyParentCertURL())
             {
                 return true;
@@ -200,7 +144,7 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
         /// <returns>검증 결과</returns>
         private bool VerifyParentCertURL()
         {
-            string url = _parentCertURL;
+            string url = GetParentCertURL();
             if (url.Equals("null"))
             {
                 return false;
@@ -245,25 +189,27 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
         /// <returns>검사 결과</returns>
         private bool VerifyParentCertWithStore()
         {
-            // 현재는 같은 것이 있는지를 CN값만 가지고 찾지만 추가적인 방법도 만들어서 정확성을 높여야 함.
-
             string storeCert;
-            string mainCert = _mainCert.IssuerName.Name;
-
-            Debug.Assert(mainCert != null, "mainCert != null");
-            var index = mainCert.IndexOf(",", StringComparison.Ordinal);
-            // 인증서의 CN 값만 추출함
-            mainCert = index > -1 ? mainCert.Substring(3, index - 3) : mainCert.Substring(3, mainCert.Length - 3);
-
+            string mainCert;
+            int index;
             foreach (var cert in _certificateStore.IntermediateCertList)
             {
                 storeCert = cert.SubjectName.Name;
                 if (storeCert != null)
                 {
                     index = storeCert.IndexOf(",", StringComparison.Ordinal);
-                    storeCert = index > -1 ? storeCert.Substring(3, index - 3) : storeCert.Substring(3, storeCert.Length - 3);
-                    if (!storeCert.Equals(mainCert)) continue;
+                    storeCert = storeCert.Substring(3, index - 3);
+
+                    mainCert = _mainCert.IssuerName.Name;
+                    if (mainCert != null)
+                    {
+                        index = mainCert.IndexOf(",", StringComparison.Ordinal);
+                        mainCert = mainCert.Substring(3, index - 3);
+
+                        if (!storeCert.Equals(mainCert)) continue;
+                    }
                 }
+                //PrintLineConsole("부모 인증서가 로컬 저장소에 존재합니다.");
                 AddCertNote("부모 인증서가 로컬 저장소에 존재합니다.");
 
                 if (cert.Verify()) return true;
@@ -274,10 +220,18 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
                 if (storeCert != null)
                 {
                     index = storeCert.IndexOf(",", StringComparison.Ordinal);
-                    storeCert = index > -1 ? storeCert.Substring(3, index - 3) : storeCert.Substring(3, storeCert.Length - 3);
+                    storeCert = storeCert.Substring(3, index - 3);
 
-                    if (!storeCert.Equals(mainCert)) continue;
+                    mainCert = _mainCert.IssuerName.Name;
+                    if (mainCert != null)
+                    {
+                        index = mainCert.IndexOf(",", StringComparison.Ordinal);
+                        mainCert = mainCert.Substring(3, index - 3);
+
+                        if (!storeCert.Equals(mainCert)) continue;
+                    }
                 }
+                //PrintLineConsole("부모 인증서가 로컬 저장소에 존재합니다.");
                 AddCertNote("부모 인증서가 로컬 저장소에 존재합니다.");
 
                 if (cert.Verify()) return true;
@@ -288,15 +242,23 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
                 if (storeCert != null)
                 {
                     index = storeCert.IndexOf(",", StringComparison.Ordinal);
-                    storeCert = index > -1 ? storeCert.Substring(3, index - 3) : storeCert.Substring(3, storeCert.Length - 3);
+                    storeCert = storeCert.Substring(3, index - 3);
 
-                    if (!storeCert.Equals(mainCert)) continue;
+                    mainCert = _mainCert.IssuerName.Name;
+                    if (mainCert != null)
+                    {
+                        index = mainCert.IndexOf(",", StringComparison.Ordinal);
+                        mainCert = mainCert.Substring(3, index - 3);
+
+                        if (!storeCert.Equals(mainCert)) continue;
+                    }
                 }
+                //PrintLineConsole("부모 인증서가 로컬 저장소에 존재합니다.");
                 AddCertNote("부모 인증서가 로컬 저장소에 존재합니다.");
 
                 if (cert.Verify()) return true;
             }
-            AddCertNote("부모 인증서가 로컬 저장소에 없습니다.");
+            AddCertNote("부모 인증서가 유효하지 않습니다.");
             return false;
         }
         /// <summary>
@@ -310,17 +272,14 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
             int start = rawData.IndexOf("Authority Info Access", StringComparison.Ordinal);
             if (start < 0)
             {
-                /* 부모 인증서 주소를 얻는 곳이 VerifyAdvanced()와 GetCertInfo() 두 곳이라서
-                 * 함수는 ExtractCert()에서 한 번만 호출시키고 대신 그 때 멤버변수
-                 * _parentCertURL에 주소를 저장해둔다.
-                 */
+                //PrintLineConsole("부모 인증서의 URL이 존재하지 않습니다.");
                 AddCertNote("부모 인증서의 URL이 존재하지 않습니다.");
                 return "null";
             }
             rawData = rawData.Substring(start, rawData.Length - start);
 
             Match match = Regex.Match(rawData, @"URL=(.{0,}crt)");
-            if (match.Groups.Count > 1) return match.Groups[1].Value;
+            if (match.Groups.Count >= 1) return match.Groups[1].Value;
 
             AddCertNote("부모 인증서의 URL이 CRT파일을 가리키고 있지 않습니다. 추가 구현이 필요합니다.");
             return "null";
@@ -349,14 +308,14 @@ namespace AuthenticodeVerifierTest.AuthenticodeVerifier
             CertificateInfo.DateExpiry = _mainCert.GetExpirationDateString();
             CertificateInfo.Subject = _mainCert.SubjectName.Name;
             CertificateInfo.Issuer = _mainCert.IssuerName.Name;
-            CertificateInfo.IssuerCertificateURL = _parentCertURL;
+            CertificateInfo.IssuerCertificateURL = GetParentCertURL();
         }
 
         private string _targetPath;
+        private X509Certificate _basicCert;
         private X509Certificate2 _mainCert;
         private X509Chain _keyChain;
         private readonly CertificateStore _certificateStore;
-        private string _parentCertURL = "null";
 
         public bool ResultPrimary { get; set; }
         public bool ResultAdvanced { get; set; }
--- AuthenticodeVerifierTest/Program.cs
+++ AuthenticodeVerifierTest/Program.cs
@@ -8,25 +8,21 @@ namespace AuthenticodeVerifierTest
         {
             Console.WriteLine("[[디지털 서명 검사 테스트]]");
             var verifier = new AuthenticodeVerifier.AuthenticodeVerifier();
-            //verifier.LoadTarget(@"C:\temp\dbk32.sys");
-            //Console.WriteLine(verifier.Verify());
-            //verifier.CertificateInfo.PrintInfo();
-            //verifier.CounterCertificateInfo.PrintInfo();
+            verifier.LoadTarget(@"C:\temp\dbk32.sys");
+            Console.WriteLine(verifier.Verify());
+            verifier.CertificateInfo.PrintInfo();
 
-            //verifier.LoadTarget(@"C:\temp\msvcr110.dll");
-            //Console.WriteLine(verifier.Verify());
-            //verifier.CertificateInfo.PrintInfo();
-            //verifier.CounterCertificateInfo.PrintInfo();
+            verifier.LoadTarget(@"C:\temp\msvcr110.dll");
+            Console.WriteLine(verifier.Verify());
+            verifier.CertificateInfo.PrintInfo();
 
-            //verifier.LoadTarget(@"C:\temp\siegfried.exe");
-            //Console.WriteLine(verifier.Verify());
-            //verifier.CertificateInfo.PrintInfo();
-            //verifier.CounterCertificateInfo.PrintInfo();
+            verifier.LoadTarget(@"C:\temp\siegfried.exe");
+            Console.WriteLine(verifier.Verify());
+            verifier.CertificateInfo.PrintInfo();
 
-            //verifier.LoadTarget(@"C:\temp\COMDLG32.OCX");
-            //Console.WriteLine(verifier.Verify());
-            //verifier.CertificateInfo.PrintInfo();
-            //verifier.CounterCertificateInfo.PrintInfo();
+            verifier.LoadTarget(@"C:\temp\COMDLG32.OCX");
+            Console.WriteLine(verifier.Verify());
+            verifier.CertificateInfo.PrintInfo();
         }
     }
 }
